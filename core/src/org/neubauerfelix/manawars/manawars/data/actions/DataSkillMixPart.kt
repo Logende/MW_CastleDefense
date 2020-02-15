8b3a556a0bb2d46a153a5d0a714e8a34998c4e2e
Felix Neubauer <felix@neuby.de>
2020-02-15 12:32:37 +0100

    removed random number support from skill mix and expanded idle support

--- core/src/org/neubauerfelix/manawars/manawars/data/actions/DataSkillMixPart.kt
+++ core/src/org/neubauerfelix/manawars/manawars/data/actions/DataSkillMixPart.kt
@@ -3,25 +3,33 @@ package org.neubauerfelix.manawars.manawars.data.actions
 
 import org.neubauerfelix.manawars.game.entities.IMovable
 import org.neubauerfelix.manawars.manawars.entities.IActionUser
+import org.neubauerfelix.manawars.manawars.entities.MSkill
 import org.neubauerfelix.manawars.manawars.handlers.MathUtils
 
-class DataSkillMixPart(val action: IDataSkill, private val offsetX: String, private val offsetY: String, private val offsetSpeedX: String, private val offsetSpeedY: String) {
+class DataSkillMixPart(val action: IDataSkill, private val offsetX: String, private val offsetY: String,
+                       private val offsetSpeedX: String, private val offsetSpeedY: String,
+                       private val offsetIdleTime: String) {
 
 
     fun spawnSkill(owner: IActionUser): Boolean {
         val s = action.spawnSkill(owner)
 
-        val offsetX = MathUtils.calcInt(this.offsetX, "r", Math.random())
-        val offsetY = MathUtils.calcInt(this.offsetY, "r", Math.random())
+        val offsetX = MathUtils.calcInt(this.offsetX)
+        val offsetY = MathUtils.calcInt(this.offsetY)
 
         s.setLocation(s.x + offsetX * s.propertyScale, s.y + offsetY * s.propertyScale)
 
-        val offsetSpeedX = MathUtils.calc(this.offsetSpeedX, "r", Math.random()).toFloat()
-        val offsetSpeedY = MathUtils.calc(this.offsetSpeedY, "r", Math.random()).toFloat()
+        val offsetSpeedX = MathUtils.calc(this.offsetSpeedX).toFloat()
+        val offsetSpeedY = MathUtils.calc(this.offsetSpeedY).toFloat()
+
+        val offsetIdleTime = MathUtils.calc(this.offsetIdleTime).toFloat()
 
         if (s is IMovable) {
             s.speedX = (s.speedX + offsetSpeedX * s.propertyScale)
             s.speedY = (s.speedY + offsetSpeedY * s.propertyScale)
+        }
+        if (s is MSkill) {
+            s.idleTimeLeft += offsetIdleTime
         }
         return true
     }
