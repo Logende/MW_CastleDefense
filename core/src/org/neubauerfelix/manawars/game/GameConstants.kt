14ed99450b728174ab9ba9d260cc82d1a315823b
Felix Neubauer <felix@neuby.de>
2020-04-30 21:21:02 +0200

    removed determinism debug comments

--- core/src/org/neubauerfelix/manawars/game/GameConstants.kt
+++ core/src/org/neubauerfelix/manawars/game/GameConstants.kt
@@ -6,74 +6,13 @@ class GameConstants {
     companion object {
         // TODO: Outsource to configuration java object
         const val DEBUG_BOUNDING_BOXES = false
-        const val FAST_MODE = true // disables graphics and audio and speeds game up
+        const val FAST_MODE = false // disables graphics and audio and speeds game up
         const val EXIT_APP_ON_CASTLE_DEFEAT = true
         const val SPEED_FACTOR_DEFAULT = 1f
         const val SPEED_FACTOR_FAST_MODE = 1f // does not change tick amount but delta per tick
-        const val GAME_TICK_FACTOR = 200f // does change simulation executions per tick (with fix delta)
+        const val GAME_TICK_FACTOR = 1f // does change simulation executions per tick (with fix delta)
         const val SLOW_INSTEAD_STUTTER = false
         const val GAME_RENDER_FIX_TIME_STEPS_DURATION = 1f/60f
-        // tick factor 10 and fast_mode = true:
- // units A: 12.0. units B: 2.0. castle health A: 154.0 castle health B: 0.0
-        // units A: 9.0. units B: 1.0. castle health A: 112.0 castle health B: 0.0
-        // units A: 12.0. units B: 2.0. castle health A: 154.0 castle health B: 0.0
-        // units A: 8.0. units B: 3.0. castle health A: 326.0 castle health B: 0.0
-        // units A: 12.0. units B: 2.0. castle health A: 154.0 castle health B: 0.0
-        // units A: 12.0. units B: 2.0. castle health A: 154.0 castle health B: 0.0
-        // tick factor 20:
-        // units A: 12.0. units B: 3.0. castle health A: 2.0 castle health B: 0.0
-        // units A: 8.0. units B: 3.0. castle health A: 326.0 castle health B: 0.0
-        // units A: 8.0. units B: 3.0. castle health A: 326.0 castle health B: 0.0
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        //tick factor 20 after changes of cosmetical maps
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // tick factor 21
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // tick factor 10
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0 -> Is the game deterministic now?
-        // tick factor 100
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // tick factor 200
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // tick factor 500
-        // units A: 12.0. units B: 1.0. castle health A: 226.0 castle health B: 0.0
-        // tick factor 6 and fast mode = false:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 -> seems like drawing screws sth up
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0
-        // tick factor 10:
-        // units A: 9.0. units B: 2.0. castle health A: 274.0 castle health B: 0.0
-        // units A: 5.0. units B: 11.0. castle health A: 0.0 castle health B: 106.0
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0
-        // units A: 9.0. units B: 2.0. castle health A: 274.0 castle health B: 0.0
-        // tick factor 30:
-        // units A: 9.0. units B: 1.0. castle health A: 456.0 castle health B: 0.0
-        // units A: 9.0. units B: 1.0. castle health A: 456.0 castle health B: 0.0
-        // units A: 9.0. units B: 1.0. castle health A: 456.0 castle health B: 0.0
-        // units A: 9.0. units B: 1.0. castle health A: 456.0 castle health B: 0.0
-        // tick factor 31:
-        // units A: 7.0. units B: 0.0. castle health A: 354.0 castle health B: 0.0
-        // units A: 7.0. units B: 0.0. castle health A: 354.0 castle health B: 0.0 - time 244064
-        // tick factor 32:
-        // units A: 8.0. units B: 1.0. castle health A: 322.0 castle health B: 0.0 - time 222960
-        // tick factor 32 after minor changes:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // tick factor 35:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // tick factor 30:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // tick factor 10:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // tick factor 30 and fast mode = true:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
-        // tick factor 300:
-        // units A: 9.0. units B: 0.0. castle health A: 294.0 castle health B: 0.0 - time 265360
 
         const val SCREEN_WIDTH = 1920f
         const val SCREEN_HEIGHT = 1080f
